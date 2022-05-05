89b9981fdab0cd0c232921f35c89f79abb74ad52
Jody Dahmer <jodydahmer@gmail.com>
2022-05-04 21:06:49 -0400

    Testing Jupyter notebook

--- scooter_opendata.py
+++ scooter_opendata.py
@@ -1,1 +1,22 @@
 #This is a test of python script for parsing Louisville scooter data
+
+#To do this , I will need to pull data from Lou Open Data Dockless Vehicles , Carto & OpenStreetsMap
+
+#Jupyter Notebooks will be used to visualize data
+
+!pip install osm-runner
+
+
+import time
+
+from osm_runner import Runner  # pip install osm-runner
+import pandas as pd
+
+from arcgis.features import FeatureLayer, GeoAccessor, GeoSeriesAccessor
+from arcgis.geoenrichment import enrich
+from arcgis import dissolve_boundaries
+from arcgis.geometry import project
+from arcgis.gis import GIS
+
+# Organization Login
+gis = GIS('home')
